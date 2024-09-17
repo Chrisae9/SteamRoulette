3e3199073f264ee21aec9bed85365474c7fa9814
chrisae9 <chrisae9@gmail.com>
2024-09-17 09:50:57 -0400

    remove old secret

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'a_secret_key_for_session'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///app.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    STEAM_API_KEY = os.environ.get('STEAM_API_KEY') or 'DD9FF55374F50EA9AB468AD29B1AF484'
+    STEAM_API_KEY = os.environ.get('STEAM_API_KEY')
